6214e9bbc0e07e0f439e8923e1d8dfed870f5314
jtm172769 <ankdxt134@gmail.com>
2017-09-12 17:07:02 +0530

    third commit

--- ps2.py
+++ ps2.py
@@ -1,9 +1,10 @@
 ###### this is the second .py file ###########
 #!/usr/bin/python
-print 'Welcome to the Game'
-player = input ("press 1 for player_1 and 2 for the player_2")
+print 'Welcome to the Game'              #printing welcome
+player = input ("press 1 for player_1 and 2 for the player_2")  # inituialising the players
 tic = [[0 for x in range(3)] for y in range(3)] 
-print "p1 will use odd numbers"
-print
+print "p1 will use odd numbers"                    # printing the players
+print " p2 will take even numbers for playing"     #printing the player2
+chance= input('enter the place and value')
 
 ####### write your code here ##########
