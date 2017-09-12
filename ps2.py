2b1d32dd588af69c8d22e31f425c2a206d9323a4
jtm172769 <ankdxt134@gmail.com>
2017-09-12 16:52:36 +0530

    second commit

--- ps2.py
+++ ps2.py
@@ -1,3 +1,9 @@
 ###### this is the second .py file ###########
+#!/usr/bin/python
+print 'Welcome to the Game'
+player = input ("press 1 for player_1 and 2 for the player_2")
+tic = [[0 for x in range(3)] for y in range(3)] 
+print "p1 will use odd numbers"
+print
 
 ####### write your code here ##########
